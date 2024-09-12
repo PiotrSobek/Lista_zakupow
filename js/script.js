db8009c1926536898eb1d3e2434234648c1483a2
Piotr Sobek <piotr.sobek@wp.pl>
2024-09-12 15:11:46 +0200

    function bindEvents added, toggleTaskDone functions and buttons added

--- js/script.js
+++ js/script.js
@@ -1,45 +1,14 @@
 {
     const tasks = [
-
         {
             content: "banany",
             done: false,
         },
-
         {
             content: "ziemniaki",
             done: true,
         },
-
     ];
-
-
-    
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li${task.done ? " style=\"text-decoration: line-through\"": ""}
-                >
-                    <button class="js-remove">usuń</button>
-                    ${task.content}
-                </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(index)
-
-            } )
-        })
-    };
-
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
@@ -48,11 +17,54 @@
 
         render();  
     }
-
-    const removeTask = (index) => {
-        tasks.splice(index, 1);
+    
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
         render();
     }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index)
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index)
+            });
+        });        
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li
+                    ${task.done ? " style=\"text-decoration: line-through\"": ""}
+                >
+                    <button class="js-done">zrobione?</buton>
+                    <button class="js-remove">usuń</button>
+                    ${task.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    
+        bindEvents();
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -63,8 +75,7 @@
             return;
         }
 
-        addNewTask(newTaskContent);
-                 
+        addNewTask(newTaskContent);                 
     };
 
     const init = () => {
@@ -76,5 +87,4 @@
     };
 
     init()
-
 }
