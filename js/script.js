ba6f70010bb4100a9a74e3e1452892a0004a79d3
Piotr Sobek <piotr.sobek@wp.pl>
2024-09-12 14:30:11 +0200

    Remove tasks

--- js/script.js
+++ js/script.js
@@ -20,7 +20,9 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li${task.done ? " style=\"text-decoration: line-through\"": ""}>
+                <li${task.done ? " style=\"text-decoration: line-through\"": ""}
+                >
+                    <button class="js-remove">usuń</button>
                     ${task.content}
                 </li>
             `;
@@ -28,15 +30,28 @@
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index)
+
+            } )
+        })
     };
 
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
-        content: newTaskContent,
+            content: newTaskContent,
         });
 
         render();  
+    }
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
     }
 
     const onFormSubmit = (event) => {
